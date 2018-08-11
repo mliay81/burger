f9431d11b0ccc6d10b1fb1fe64201130ef41739e
mliay81 <mikeliay@gmail.com>
2018-08-10 22:46:38 -0500

    setup

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+-- Insert a set of records.
+INSERT INTO burgers_db (burger_name, devoured) VALUES ("New Bacon-ings Burger", false);
+INSERT INTO burgers_db (burger_name, devoured) VALUES ("The Foot Feta-ish Burger", false);
+INSERT INTO burgers_db (burger_name, devoured) VALUES ("Chevre Which Way But Loose Burger", false);
