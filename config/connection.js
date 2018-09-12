58d2ee5a635007c69a6c9fb9def33ef7aa6ba692
mliay81 <mikeliay@gmail.com>
2018-09-11 22:07:29 -0500

    modify config

--- config/connection.js
+++ config/connection.js
@@ -7,9 +7,13 @@ var connection = mysql.createConnection({
   password: "root",
   database: "burgers_db"
 });
-
+var devConfig = {Host:	"muowdopceqgxjn2b.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",	
+Username:	"igg7l05onzhry7xp",	
+Password:	"b5n347gtpznxqa5t",	
+Port:	3306,	
+Database:	"zk1la99eeg0qjo1t"}
 if (process.env.JAWSDB_URL) {
-  connection = mysql.createConnection(process.env.JAWSDB_URL)
+  connection = mysql.createConnection(devConfig)
 } else {
   connection = mysql.createConnection({
     host: "localhost",
